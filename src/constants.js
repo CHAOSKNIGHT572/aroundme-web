8dd794df7cc1bb6d56fc6678bbe0224f747cf7f8
CHAOSKNIGHT572 <stan04@syr.edu>
2018-03-31 17:13:00 -0400

    hard code position

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
 export const API_ROOT = 'https://around-75015.appspot.com';//'http://our-mechanism-194922.appspot.com';
 export const TOKEN_KEY = 'TOKEN_KEY';
+export const GEO_OPTIONS = { enableHighAccuracy:true, maximumAge:30000, timeout:27000 };
+export const AUTH_PREFIX = `Bearer`;
